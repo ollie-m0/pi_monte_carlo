358ade437c2e7bd4979c0d01869e732ed9ba7226
ollie-m0 <olliemiller500@protonmail.com>
2026-02-20 17:22:07 +0000

    Add files via upload

---
+++ predict_pi_2d.cpp
@@ -0,0 +1,57 @@
+/*
+ This code uses a Monte-Carlo technique to estimate a numerical value of pi.
+*/
+
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <cmath>
+
+using namespace std;
+
+double generateRadius_2D()
+{
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+
+    // Generate pseudo-random numbers in the range [-1, 1].
+    uniform_real_distribution<double> distribution (-1.0, 1.0);
+
+    double x = distribution(generator);
+    double y = distribution(generator);
+
+    // Using the Pythagorean theorem to return distance of (x, y) from the origin.
+    double radius = sqrt(x*x + y*y);
+
+    return radius;
+}
+
+double predictPi(const int num)
+{
+    double numInCircle;
+
+    for (int i = 0; i < num; i++)
+    {
+        double r = generateRadius();
+
+        if (fabs(r) <= 1)
+        {
+            numInCircle += 1;
+        }
+    }
+
+    double pi = numInCircle * 4 / (double)num;
+
+    return pi;
+}
+
+
+int main()
+{
+    int n = 10000;
+    double pi = predictPi(n);
+
+    cout << "pi is approximately equal to " << pi << endl;
+
+    return 0;
+}
