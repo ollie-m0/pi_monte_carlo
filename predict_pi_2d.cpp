d1a9b1cfe684b3dd195faebb3b03b70fbc00d3c3
ollie-m0 <olliemiller500@protonmail.com>
2026-03-19 19:46:51 +0000

    .

--- predict_pi_2d.cpp
+++ predict_pi_2d.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-double generateRadius_2D()
+double generateRadius()
 {
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     default_random_engine generator (seed);
